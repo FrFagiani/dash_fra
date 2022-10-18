17ec6858097760155994373db2eb189a1664f515
Francesco <fr.fagiani@gmail.com>
2022-10-18 16:11:43 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 # import geopandas as gpd
 # from shapely import wkt
-import plotly.graph_objects as go
+import plotly.express as px
 
 st.set_page_config(layout="wide")
 st.title('How to visualize and filter dataframe')
